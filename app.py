89682a04c442262b99ee87b04d907e29d09ea399
AbhinavS8 <abhinav.subr@gmail.com>
2025-04-04 23:27:19 +0530

    added routes

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask,url_for,redirect,render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return render_template("index.html",name = "world")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/post/<int:id>')
+def post(id):
+    # show the post with the given id, the id is an integer
+    return 'Post '+str(id)
+
+# @app.route('/projects/')
+# def projects():
+#     return 'The project page'
+
+@app.route('/projects')
+def projects():
+    return 'The skbi page'
+
+@app.route("/top_post")
+def show_top_post():
+    return redirect(url_for("post",id=1)) #instead of redirect(/post/1)
