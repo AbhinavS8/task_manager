c8a3d091c97df8ec14bb21b3da436b0ac6ba07de
AbhinavS8 <abhinav.subr@gmail.com>
2025-04-11 17:08:56 +0530

    added completion functionality

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-@app.route("/home")
-def index():
-    return render_template("index.html")
+# @app.route("/home")
+# def index():
+#     return render_template("index.html")
 
 @app.route("/login")
 def login():
@@ -27,7 +27,7 @@ def schedule():
 @app.route("/tasks/")
 def list_tasks():
     overdue_tasks = Task.query.order_by(Task.due_time).filter(Task.due_time < datetime.now()).all()
-    normal_tasks = Task.query.order_by(Task.due_time).filter(Task.due_time < datetime.now()).all()
+    normal_tasks = Task.query.order_by(Task.due_time).filter(Task.completed == False).filter(Task.due_time < datetime.now()).all()
     return render_template('task_list.html', tasks=normal_tasks,o_tasks=overdue_tasks)
 
 @app.route('/tasks/create', methods=['GET', 'POST'])
@@ -37,17 +37,18 @@ def create_task():
         task_category = request.form['task_category']
         start_time = datetime.strptime(request.form['start_time'], '%Y-%m-%dT%H:%M')
         due_time = datetime.strptime(request.form['due_time'], '%Y-%m-%dT%H:%M')
-
+        task_description = request.form['task_description']
         task = Task(
             task_name=task_name,
             task_category=task_category,
             start_time=start_time,
-            due_time=due_time
+            due_time=due_time,
+            task_description=task_description
         )
 
         db.session.add(task)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for('list_tasks'))
     return render_template("create_task.html")
 
 @app.route("/tasks/<int:id>")
@@ -60,9 +61,16 @@ def completed_tasks():
     return "<p>Completed tasks page</p>"
 
 
-# @app.route("/")
-# def hello_world():
-#     return render_template("index",name = "hello world")
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route('/tasks/<int:id>/complete', methods=['POST'])
+def complete_task(id):
+    task = Task.query.get_or_404(id)
+    task.completed = True
+    db.session.commit()
+    return redirect(url_for('task_detail', id=id))
 
 # @app.route('/post/<int:id>')
 # def post(id):
--- models.py
+++ models.py
@@ -12,3 +12,5 @@ class Task(db.Model):
     task_category = db.Column(db.VARCHAR(255), nullable=False)
     start_time = db.Column(db.TIMESTAMP, nullable=False)
     due_time = db.Column(db.TIMESTAMP, nullable=False)
+    task_description = db.Column(db.Text, nullable=True) 
+    completed = db.Column(db.Boolean, default=False)
